e7e72cd06e70a2ee56167405f98b5a02c9f9014e
mbiacharetyron <mbiacharetyron@gmail.com>
2023-11-23 09:45:38 +0100

    More implementations on course management.

---
+++ apis/assessment/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+# Create a router
+router = DefaultRouter()
+
+# Register the StudentViewSet with a base name 'student'
+# router.register(r'assessment', StudentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('create_assessment/', create_assessment, name='create_assessment'),
+    path('assessments/<int:assessment_id>/add_question/', create_question_with_choices, name='add_question'),
+]
--- apis/students/urls.py
+++ apis/students/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('register_course/<str:course_id>/', register_course, name='register_course'),
     path('drop_course/<str:course_id>/', drop_course, name='drop_course'),
     path('registered_courses/', get_registered_courses, name='get_registered_courses'),
-    # path('view_course_materials/<str:course_id>/', view_course_materials, name='view_course_materials'),
+    path('view_course_materials/<str:course_id>/', view_course_materials, name='view_course_materials'),
 ]
